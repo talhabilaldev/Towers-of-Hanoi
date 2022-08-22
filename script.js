b4d8004b6fc0e28fd08da5c616c533a308788199
Talha Bilal <talhabilal1997@gmail.com>
2022-08-22 18:47:10 +0500

    Some more changes

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ function stopGame() {
             }
         }
     }
+    emptyArrays(undoArray);
+    emptyArrays(redoArray);
 }
 function enableDraggable(diskID) {
     $("#" + diskID).draggable({
